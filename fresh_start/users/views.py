0c5b2279235c5d8a4ed71d5931fada58d46520c2
dep <depena210@gmail.com>
2022-01-21 17:15:00 -0500

    Added to views

--- fresh_start/users/views.py
+++ fresh_start/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
 def register(request):
