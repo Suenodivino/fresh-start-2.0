dc95961c5a125ad6bea74e975b958bc77cd87087
dep <depena210@gmail.com>
2022-01-27 22:23:48 -0500

    Added to urls.py, apps.py, settings.py, profile.html, and created signals.py and media directory

--- fresh_start/mysite/urls.py
+++ fresh_start/mysite/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='mysite-home'),
@@ -13,3 +15,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
